ba12f4440ac4715c760dad5a35eaa7bc4a28a81a
unknown <mailid>
2022-12-20 16:16:11 +0530

    first commit

---
+++ src/test/java/com/jenkins/framework/PracticeJenkinsTest.java
@@ -0,0 +1,10 @@
+package com.jenkins.framework;
+
+import org.testng.annotations.Test;
+
+public class PracticeJenkinsTest {
+@Test(groups = "sanity")
+public void test1Test() {
+	System.out.println("hello");
+}
+}
