47e3f9ade6a07e8fdbfc20953dd3be5347f53e27
unknown <mailid>
2022-12-21 10:39:23 +0530

    update regression test

---
+++ src/test/java/com/jenkins/framework/practiceJenkins2Test.java
@@ -0,0 +1,12 @@
+package com.jenkins.framework;
+
+import org.testng.annotations.Test;
+
+public class practiceJenkins2Test {
+@Test(groups = {"regression","sanity"})
+	
+public void test2Test() {
+	System.out.println("welcome");
+	
+}
+}
