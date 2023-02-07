f374dd9e5949250a75a68b213a843d0e794c7080
pk123 <mailid>
2023-02-07 17:15:27 +0530

    commit now

---
+++ src/test/java/com/jenkins/framework/SetJenkinsParameter2Test.java
@@ -0,0 +1,12 @@
+package com.jenkins.framework;
+
+import org.testng.annotations.Test;
+
+public class SetJenkinsParameter2Test {
+	@Test
+public void recieveParameterFromJenkins() {
+	String BROWSER=System.getProperty("browser");
+	String ENV=System.getProperty("url");
+	System.out.println("Browser===>"+BROWSER+" ,Url==>"+ENV);
+}
+}
