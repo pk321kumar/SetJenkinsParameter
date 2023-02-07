fc670bb1053b5012bc5e87f37c3f26e7e0477376
pk123 <mailid>
2023-02-07 17:34:59 +0530

    change

--- src/test/java/com/jenkins/framework/SetJenkinsParameter2Test.java
+++ src/test/java/com/jenkins/framework/SetJenkinsParameter2Test.java
@@ -4,10 +4,10 @@ import org.testng.annotations.Test;
 
 public class SetJenkinsParameter2Test {
 	@Test
-public void recieveParameterFromJenkins() {
-	String BROWSER=System.getProperty("browser");
-	String ENV=System.getProperty("url");
-	System.out.println("Browser===>"+BROWSER+" ,Url==>"+ENV);
-	//..
-}
+	public void recieveParameterFromJenkins() {
+		String BROWSER=System.getProperty("browser");
+		String ENV=System.getProperty("url");
+		System.out.println("Browser===>"+BROWSER+" ,Url==>"+ENV);
+		//..hello
+	}
 }
