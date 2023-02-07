1ad09a707ca2977006ae464889387f12421e9a0c
pk123 <mailid>
2023-02-07 16:06:47 +0530

    added some feature

--- src/test/java/com/jenkins/framework/SetJenkinsParameterTest.java
+++ src/test/java/com/jenkins/framework/SetJenkinsParameterTest.java
@@ -1,11 +1,21 @@
 package com.jenkins.framework;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class SetJenkinsParameterTest {
 	@Test
 public void recieveParameterFromJenkins() {
 	String BROWSER=System.getProperty("browser");
 	System.out.println(BROWSER);
+	if(BROWSER.equals("chrome"))
+	{
+	WebDriverManager.chromedriver().setup();
+	WebDriver driver=new ChromeDriver();
+	}
+	
 }
 }
